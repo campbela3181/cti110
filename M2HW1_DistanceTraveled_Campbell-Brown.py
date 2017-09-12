c79857cd6c39d2cfd4e372b138bdfb067f74ad1c
Alicia Campbell-Brown <31824882+campbela3181@users.noreply.github.com>
2017-09-12 02:48:54 -0400

    Add files via upload
    Distance Traveled Assignment

---
+++ M2HW1_DistanceTraveled_Campbell-Brown.py
@@ -0,0 +1,14 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# Alicia Campbell-Brown
+# 9-10-2017
+
+
+speed = 70
+time1 = 6
+time2 = 10
+time3 = 15
+
+print ("In 6 hours, the car will travel..." +  str( (speed * time1) ) + " miles" )
+print ("In 10 hours, the car will travel..." +  str( (speed * time2) ) + " miles" )
+print ("In 15 hours, the car will travel..." +  str( (speed * time3) ) + " miles" )
