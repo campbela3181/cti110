48df8b31872ad8c8d97b7b82115c4ed531db5a96
Alicia Campbell-Brown <31824882+campbela3181@users.noreply.github.com>
2017-11-11 11:51:45 -0500

    Add files via upload
    Feet to inches program

---
+++ M6T2_FeetToInches_Campbell-Brown.py
@@ -0,0 +1,21 @@
+# 11-11-2017
+# CTI-110 M6T2_FeetToInches 
+# Alicia Campbell-Brown
+#Write a program that asks the user to enter a distance in feet,
+#and prints that same distance in inches. 
+#This is the conversion formula, of course:
+#Inches = Feet * 12
+
+
+def feetToInches( userFeet ):
+    inches = ( userFeet / 1 ) * 12
+    return inches
+
+def main():
+    userFeet = float( input( "Please enter the number of feet: " ) )
+    print()
+    inches = feetToInches( userFeet )
+    print( userFeet, "feet converted to inches is", format( inches, ".2f" ),\
+           "inches" )
+
+main()
